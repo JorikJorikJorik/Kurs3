4980d97208b799efe44d7843a9708da44a809613
jorikEmelyanov <emelyanovjora95@gmial.com>
2016-04-15 17:06:57 +0300

    Теперь точно добавил

---
+++ KursWebApplication/KursWebApplication/Business Logic/BusLogic.cs
@@ -0,0 +1,37 @@
+﻿using KursWebApplication.Data_Access;
+using KursWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KursWebApplication.Business_logic
+{
+    public class BusLogic
+    {
+        BusDataAccess dataAccess = new BusDataAccess();
+
+        public List<MyDBModels.Bus> logicMethodForGetListData()
+        {
+            List<MyDBModels.Bus> listData = dataAccess.getListData();
+            return listData;
+        }
+
+        public MyDBModels.Bus logicMethodForGetData(int id)
+        {
+            MyDBModels.Bus data = dataAccess.getDataById(id);
+            return data;
+        }
+
+        public void logicMethodForPostData(Models.BusModel data)
+        {
+             dataAccess.postBus(data);
+        }
+
+        public void logicMethodForDeleteDataint(int id)
+        {
+            dataAccess.deleteBus(id);
+        }
+
+    }
+}
---
+++ KursWebApplication/KursWebApplication/Data Access/BusDataAccess.cs
@@ -0,0 +1,48 @@
+﻿using KursWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KursWebApplication.Data_Access
+{
+    public class BusDataAccess
+    {
+        public List<MyDBModels.Bus> getListData()
+        {
+            var db = new MyDBModels.DB();
+            List<MyDBModels.Bus> listData = db.bus.ToList();
+            return listData;
+        }
+
+        public MyDBModels.Bus getDataById(int id)
+        {
+            var db = new MyDBModels.DB();
+            MyDBModels.Bus busModel = db.bus.Where(b => b.BusId == id).FirstOrDefault();
+            return busModel;
+        }
+
+        public void postBus(Models.BusModel newbus)
+        {
+            var db = new MyDBModels.DB();
+            MyDBModels.Bus bus = new MyDBModels.Bus();
+            bus.BusCondition = newbus.Condition;
+            bus.BusNumber = newbus.BusNomber;
+            bus.Model = newbus.Model;
+            db.bus.Add(bus);
+            db.SaveChanges();
+        }
+
+
+        public void deleteBus(int id)
+        {
+            var db = new MyDBModels.DB();
+            MyDBModels.Bus bus = db.bus.Where(b => b.BusId == id).FirstOrDefault();
+            if (bus != null)
+            {
+                db.bus.Remove(bus);
+                db.SaveChanges();
+            }
+        }
+    }
+}
